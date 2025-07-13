6d0da721249e662b8fd089ddc6b52f3444e1e2a7
victor.agbeo <vicagbeo@outlook.com>
2025-07-12 18:50:23 -0700

    Update payvic.swift

--- payvic.swift
+++ payvic.swift
@@ -1,10 +1,6 @@
 //
 //  payvic.swift
 //  
-//Minister decided to proclaim an independent Bavaria and its secession from the German Republic. This was to have taken place on the eve of the fifth anniversary of the establishmentr of rthe German Republic.     This was to take place on the eve of    Fith anniversary of the estanlishment   Nov 9 1918. Hitler staged a counter stroke. For several days he had been mobilizin his storm battalions in the neighbourhood of Munich, intending to make a national demonstration and hoping that the REICHSWEHR would stand by him to prevent secession. Ludendorff was with him. And he thought that the prestige of the great German Commander in the World War would be sufficient to win the allegiance of the professional army.
-
-A meeting had been announced to take place in the bürgerbräu KELLER ON THE night of November 8rh. The Bavarian patriotic societies were gathered there, and the Prime Minister, Dr. von Kahr, start to read his official PRONUNCIAMENTO which practically amounted to a proclamation of Bavarian independence and secession from the Republic;  //
-//  Untitled.swift
 //
 //
 //  Created by Victor Agbeo on 7/12/25.
